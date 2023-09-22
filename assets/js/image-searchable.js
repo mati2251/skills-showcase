98e5ae05bf804c4cca8584b12d29887266a00d6b
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-09-22 22:35:21 +0200

    Added image searchable

--- assets/js/image-searchable.js
+++ assets/js/image-searchable.js
@@ -1,1 +1,11 @@
-console.log("image-searchable.js")
+const images = document.querySelectorAll('img');
+images.forEach(image => {
+    const alt = image.alt;
+    const isShield = image.src.includes('img.shields.io');
+    if (isShield){
+        const span = document.createElement('span');
+        span.innerText = alt;
+        span.style.fontSize = '0px';
+        image.replaceWith(image, span)       
+    }
+});
